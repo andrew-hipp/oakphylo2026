899adddc2dc8bbbaa5f20825de5bb378e05a4247
Andrew Hipp <AHipp@mortonarb.org>
2026-02-10 10:02:46 -0600

    Rogue taxon analysis (code and tables)

--- TREE-COMPARISONS/topologies/scripts/40_rogues.R
+++ TREE-COMPARISONS/topologies/scripts/40_rogues.R
@@ -1,1 +1,18 @@
 # do rogue analyses using `Rogue` package -- not written (2026-02-09)
+
+library(Rogue)
+
+if(!exists('rogues')) {
+    rogues <- list(
+        allPruned = RogueTaxa(treesAll.pruned),
+        boots = lapply(boots, function(x) {
+            lapply(x, RogueTaxa)
+        })
+    )
+    }
+
+for(i in names(rogues$boots)){
+    for(j in names(rogues$boots[[i]])){
+        write.csv(rogues$boots[[i]][[j]], paste('out/rogues_',j,'.csv', sep = ''))
+    }
+}
