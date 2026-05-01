5cd0c0f19476b621c06a1555bebd6a833330130f
andrew-hipp <andrew.hipp@gmail.com>
2026-05-01 03:47:35 -0500

    fix ordination stats

--- topologies/scripts/27_handyTreeStats.R
+++ topologies/scripts/27_handyTreeStats.R
@@ -7,14 +7,14 @@
 bootRows <- list(
     reseq = intersect(grep('reseq', row.names(monophylyMat)), grep('bt', row.names(monophylyMat))),
     simRAD = intersect(grep('simRAD', row.names(monophylyMat)), grep('bt', row.names(monophylyMat))),
-    reSim = intersect(grep('simRAD|reseq', row.names(monophylyMat)), grep('bt', row.names(monophylyMat))),
-    empiricalRAD = intersect(grep('empiricalRAD', row.names(monophylyMat)), grep('bt', row.names(monophylyMat))),
+    reseq_simRAD = intersect(grep('simRAD|reseq', row.names(monophylyMat)), grep('bt', row.names(monophylyMat))),
+    empiricalRAD = intersect(grep('empiricalRAD', row.names(monophylyMat)), grep('bt|OakPhylo', row.names(monophylyMat))),
     ref_alba = intersect(grep('ref_alba', row.names(monophylyMat)), grep('bt', row.names(monophylyMat))),
     ref_glauca = intersect(grep('ref_glauca', row.names(monophylyMat)), grep('bt', row.names(monophylyMat))),
     ref_longispica = intersect(grep('ref_longispica', row.names(monophylyMat)), grep('bt', row.names(monophylyMat))),
     ref_rubra = intersect(grep('ref_rubra', row.names(monophylyMat)), grep('bt', row.names(monophylyMat))),
     ref_tomentella = intersect(grep('ref_tomentella', row.names(monophylyMat)), grep('bt', row.names(monophylyMat))),
-    ref_variabilis = intersect(grep('ref_variablis', row.names(monophylyMat)), grep('bt', row.names(monophylyMat))),
+    ref_variabilis = intersect(grep('ref_variabilis', row.names(monophylyMat)), grep('bt', row.names(monophylyMat))),
     ref_virginiana = intersect(grep('ref_virginiana', row.names(monophylyMat)), grep('bt', row.names(monophylyMat)))
 )
 
