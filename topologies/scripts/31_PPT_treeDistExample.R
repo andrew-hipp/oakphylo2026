40ad5215b28398bd5bb637b58f49a2f7ee23d03d
Andrew Hipp <andrew-hipp@users.noreply.github.com>
2026-07-20 10:04:50 -0500

    committing treedist script

--- topologies/scripts/31_PPT_treeDistExample.R
+++ topologies/scripts/31_PPT_treeDistExample.R
@@ -1,44 +1,55 @@
 library(ape)
 library(TreeDist)
 library(ggplot2)
+library(TreeTools)
 
-tr <- rtree(5)
-trs <- c(
-    tr, 
-    rSPR(tr, 1, 2), 
-    rSPR(tr, 2, 2), 
-    rSPR(tr, 3, 2)
-)
+if(!exists('trs')) {
+    tr <- rtree(15)
+    repsPerTree <- 3
+    trs <- c(
+        tr, 
+        rSPR(tr, 1, repsPerTree), 
+        rSPR(tr, 2, repsPerTree), 
+        rSPR(tr, 3, repsPerTree)
+    )
 
-trs <- lapply(trs, multi2di)
+    trs <- lapply(trs, MakeTreeBinary)
 
-trs.d <- ClusteringInfoDistance(trs)
-trs.mds <- cmdscale(trs.d, 2) |> as.data.frame()
+    trs.d <- ClusteringInfoDistance(trs)
+    trs.mds <- cmdscale(trs.d, 2) |> as.data.frame()
+}
 
 xrange <- range(trs.mds$V1)
 yrange <- range(trs.mds$V2)
-xpad <- 0.05 * diff(xrange)
-ypad <- 0.05 * diff(yrange)
+xpad <- 0.75 * diff(xrange)
+ypad <- 0.75 * diff(yrange)
 xlim <- xrange + c(-xpad, xpad)
 ylim <- yrange + c(-ypad, ypad)
 xnorm <- (trs.mds$V1 - xlim[1]) / diff(xlim)
 ynorm <- (trs.mds$V2 - ylim[1]) / diff(ylim)
 
+pdf('out/figures/31_PPT_treeDistExample.pdf', width = 8, height = 6)
 plot(trs.mds$V1, trs.mds$V2, type = "n",
      xlim = xlim, ylim = ylim,
      xlab = "MDS1", ylab = "MDS2",
      main = "Tree positions from MDS")
-points(trs.mds$V1, trs.mds$V2, pch = 19, col = "blue")
+point.cols <- c("red", rep("blue", length(trs) - 1))
+point.pch <- c(24, rep(21, length(trs) - 1))
+point.bg <- c("red", rep("blue", length(trs) - 1))
+points(trs.mds$V1, trs.mds$V2, pch = point.pch, col = point.cols, bg = point.bg, cex = 2)
+title(sub = "Red filled triangle: original tree; blue filled circles: SPR trees", cex.sub = 0.8)
 
 old_par <- par(no.readonly = TRUE)
 for (i in seq_along(trs)) {
-    size <- 0.12
+    size <- 0.05
     fig <- c(xnorm[i] - size, xnorm[i] + size,
              ynorm[i] - size, ynorm[i] + size)
     fig[1] <- max(0, fig[1]); fig[2] <- min(1, fig[2])
     fig[3] <- max(0, fig[3]); fig[4] <- min(1, fig[4])
     par(fig = fig, new = TRUE, mar = c(0, 0, 0, 0))
+    tree.col <- ifelse(i == 1, "red", "blue")
     plot(trs[[i]], show.tip.label = FALSE, direction = "rightwards",
-         no.margin = TRUE)
+         edge.color = tree.col, tip.color = tree.col, no.margin = TRUE)
 }
 par(old_par)
+dev.off()
